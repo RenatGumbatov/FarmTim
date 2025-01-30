c3dfd2ed58c710049a5b5eddb444ae7c5cd9c1be
RenatGumbatov <rybas@spsejecna.cz>
2025-01-30 10:14:25 +0100

    added Garden.java logic

--- src/Garden.java
+++ src/Garden.java
@@ -1,14 +1,27 @@
 import flowers.Flower;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Garden {
     private int maxOfTypes = 5;
     private ArrayList<Flower> flowers;
 
     public void plantFlower(int choice) {
+        if (amountOfFlowerTypesPlanted() <= maxOfTypes) {
+            flowers.add(Flower.createFlower(choice));
+        }
     }
 
     public void waterFlower(int i) {
+        flowers.get(i).waterFlower();
+    }
+
+    public int amountOfFlowerTypesPlanted() {
+        HashSet<Class> types = new HashSet<>();
+        for (Flower flower : flowers) {
+            types.add(flower.getClass());
+        }
+        return types.size();
     }
 }
