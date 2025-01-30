0e864a53354faa86ee626ec73d6ae3d5458dbd79
RenatGumbatov <rybas@spsejecna.cz>
2025-01-30 09:49:52 +0100

    Added Barn.java, Garden.java and method headings

---
+++ src/Garden.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Garden {
+    private ArrayList<Flower> flowers;
+
+    public void plantFlower(int choice) {
+    }
+
+    public void waterFlower(int i) {
+    }
+}
