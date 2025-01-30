76039c9d12c2d454d9fa81eb7aa9b67ce474d26a
RenatGumbatov <rybas@spsejecna.cz>
2025-01-30 10:49:50 +0100

    Console draft

---
+++ src/console/Console.java
@@ -0,0 +1,42 @@
+package console;
+
+public class Console {
+    /*
+    1. Manage flowers
+    2. Manage animals
+    3. Exit
+
+    >> 1
+    1. Return
+    2. Plant flower
+    3. Water flower
+    4. Remove flower
+
+    >> 2
+    Current flowers: ...
+    Choose flower to plant:
+    1. Return
+    2. Flower1
+    3. Flower2
+    4. Flower3
+    5. Flower4
+    6. Flower5
+    7. Rose
+
+    >> 3
+    1. Return
+    2. Plant flower
+    3. Water flower
+    4. Remove flower
+
+    >> 1
+    1. Manage flowers
+    2. Manage animals
+    3. Exit
+
+    >> 2
+    1. Return
+    2. Add animal
+    3. Remove animal
+     */
+}
